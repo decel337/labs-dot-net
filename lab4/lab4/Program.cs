3e47fc8873359304647885de5505aede05df95fd
Anatolii <Parshykov>
2022-06-06 19:33:04 +0300

    +lab4

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bouquet bouquet1 = new RoseBouquet();
+            bouquet1 = new RibbonBouquet(bouquet1); 
+            Console.WriteLine("Name: {0}", bouquet1.Name);
+            Console.WriteLine("Cost: {0}", bouquet1.GetCost());
+ 
+            Bouquet bouquet2 = new TulipBouquet();
+            bouquet2 = new PackageBouquet(bouquet2);
+            Console.WriteLine("Name: {0}", bouquet2.Name);
+            Console.WriteLine("Cost: {0}", bouquet2.GetCost());
+ 
+            Bouquet bouquet3 = new LilyBouquet();
+            bouquet3 = new RibbonBouquet(bouquet3);
+            bouquet3 = new PackageBouquet(bouquet3);
+            Console.WriteLine("Name: {0}", bouquet3.Name);
+            Console.WriteLine("Cost: {0}", bouquet3.GetCost());
+        }
+    }
+}
